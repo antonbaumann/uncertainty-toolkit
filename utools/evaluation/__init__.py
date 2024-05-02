a056e3cf9d27549662039c0b25ae17c4a79d7460
Anton Baumann <anton@antonbaumann.com>
2024-05-02 11:53:19 +0200

    update documentation

--- utools/evaluation/__init__.py
+++ utools/evaluation/__init__.py
@@ -1,3 +1,10 @@
-from utools.evaluation.base import expected_calibration_error
-from utools.evaluation.classification import classification_calibration_df
-from utools.evaluation.regression import regression_calibration_df, regression_precision_recall_df
+from .base import expected_calibration_error
+from .classification import classification_calibration_df
+from .regression import regression_calibration_df, regression_precision_recall_df
+
+__all__ = [
+    'expected_calibration_error',
+    'classification_calibration_df',
+    'regression_calibration_df',
+    'regression_precision_recall_df',
+]
